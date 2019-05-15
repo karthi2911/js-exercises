5a9af477989ca1b42458f12462812d3c109410ae
Karthik <karthi2911@gmail.com>
2019-05-15 16:03:01 +0530

    Update main.js
    setting

--- 04-color-matching-game/main.js
+++ 04-color-matching-game/main.js
@@ -1,1 +1,13 @@
 //Color matching game
+let card = document.getElementById('board');
+let randomColors = ['Red', 'Green', 'cyan', 'plum', 'saddlebrown', 'teal', 'orange', 'tan' ];
+
+const displayCard = (e) => {
+    console.log(e.target);
+    const setColors = randomColors[Math.floor(Math.random()*randomColors.length)];
+    e.target.style.backgroundColor = setColors;
+}
+// loop to add event listeners to each card
+for (let i = 0; i < randomColors.length; i++){
+    card.addEventListener("click", displayCard);
+};
